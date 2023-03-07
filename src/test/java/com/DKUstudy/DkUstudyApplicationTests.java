afe037a40b8a4b6ce68640fd21489b13db566de0
soyeon <itso22@naver.com>
2023-03-07 16:16:05 +0900

    create project

---
+++ src/test/java/com/DKUstudy/DkUstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DKUstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkUstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
