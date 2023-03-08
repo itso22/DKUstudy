d5eb56683e9a2456bbaa727789809f47ea65ee3f
soyeon <itso22@naver.com>
2023-03-08 13:15:52 +0900

    현재 인증된 사용자 정보 참조

---
+++ src/main/java/com/DKUstudy/MainController.java
@@ -0,0 +1,19 @@
+package com.DKUstudy;
+
+import com.DKUstudy.account.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(@CurrentUser Account account, Model model){
+        if (account != null) {
+            model.addAttribute(account);
+        }
+        return "index";
+    }
+
+}
